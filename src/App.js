1489d1c3a2da6fd0a66fedd54327fa094b571219
sandblue <a5810164@gmail.com>
2020-09-02 21:56:48 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
+import { render } from 'react-dom';
 
-function App() {
-  return (
+class App extends Component {
+  // this react will re render when property or state has change
+  // and it will rerender only where Dom must change :COOLLLL
+  state = {
+    person: [
+      {name: 'K', age: 20},
+      {name: "Nut", age: 33}
+    ]
+  }
+
+  clickEventHandle = () =>{
+    // this ... are call spread , [and also call rest if use for 
+    // function name = (...args) => {} ...arge called rest use for collect args to list  ]
+    // this use for avoid reference value [ like copy value ] , but primitive value are pass be value
+    let personTemp = [...this.state.person]
+    // react must use this for update state and it will effect only where it should effect
+    this.setState({person:[
+      {name: personTemp[1].name, age: personTemp[1].age},
+      {name: personTemp[0].name, age: personTemp[0].age}
+    ]})
+  }
+
+  // Note : in onClick that must not have () after method because if have it will run that while
+  // react render at first time 
+  // actualy jsx here are managed by react libaly [React.createElement()] that is why we need to 
+  // import React 
+  render() { 
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello React ?!</h1>
+     <button onClick={this.clickEventHandle}>Swap data in array</button>
+     <Person name="Static Name Krub" age= "0"></Person>
+     <Person name={this.state.person[0].name} age= {this.state.person[0].age}></Person>
+     <Person name={this.state.person[1].name} age= {this.state.person[1].age}></Person>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = (props) => {
+    return (<div><p1>
+        My name is {props.name} and I'am {props.age} Year old
+    </p1></div>);
+}
+
+export default person;
